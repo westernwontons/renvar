541abba036b0ae7f02ed8fe4efb530cec1260910
Nagy Botond <75550932+westernwontons@users.noreply.github.com>
2023-05-30 22:11:29 +0300

    avoid a clone if not necessary

--- src/convert.rs
+++ src/convert.rs
@@ -444,8 +444,13 @@ pub(crate) fn maybe_invalid_unicode_vars_os(
     // we don't expect a lot of errors to happen, so it's better to just clone,
     // instead of putting a lifetime bound with a Cow or OsStr on renvar::Error
     for (key, value) in vars.iter() {
-        key.clone().map_err(Error::InvalidUnicode)?;
-        value.clone().map_err(Error::InvalidUnicode)?;
+        if let Err(key_error) = key {
+            return Err(Error::InvalidUnicode(key_error.to_owned()));
+        }
+
+        if let Err(value_error) = value {
+            return Err(Error::InvalidUnicode(value_error.to_owned()));
+        }
     }
 
     Ok(vars
